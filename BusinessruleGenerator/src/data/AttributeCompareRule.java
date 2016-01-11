56da4bdde4af43a1a0f7052dd1f4fe1369746b1a
perdignus <gemineavanimeg@gmail.com>
2016-01-11 12:55:47 +0100

    compare dao verbetert

--- BusinessruleGenerator/src/data/AttributeCompareRule.java
+++ BusinessruleGenerator/src/data/AttributeCompareRule.java
@@ -2,10 +2,31 @@ package data;
 
 import data.Typen.Type;
 
-public class AttributeCompareRule extends BusinessRule{
+public class AttributeCompareRule extends BusinessRule {
+	private String operator;
+	private int compareValue;
 
-	public AttributeCompareRule(String naam, int id, String rootTabel, String rootColom) {
+	public AttributeCompareRule(String naam, int id, String rootTabel, String rootColom, String operator,
+			int compareValue) {
 		super(naam, id, rootTabel, rootColom, Typen.Type.ATTRIBUTE_COMPARE);
+		this.operator = operator;
+		this.compareValue = compareValue;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public int getCompareValue() {
+		return compareValue;
+	}
+
+	public void setCompareValue(int compareValue) {
+		this.compareValue = compareValue;
 	}
 
 }
--- BusinessruleGenerator/src/infrastructure/AttributeCompareDAO.java
+++ BusinessruleGenerator/src/infrastructure/AttributeCompareDAO.java
@@ -13,16 +13,18 @@ public class AttributeCompareDAO extends RootDAO {
 
 	public BusinessRule getData(int id) {
 		BusinessRule br = null;
-		String query = "SELECT br.RuleName, br.BusinessRuleNr, tab.TableName, col.ColumnName,  atrr.MinimumValue, atrr.MaximumValue, attr.CompareType "
-				+ "FROM BusinessRule br, Attribute_Range_rule atrr, Column col, Table tab " + "WHERE br.RuleNr =" + id
-				+ " AND br.RuleNr = attr.RuleNr AND attr.ColumNr=col.ColumnNr AND col.TableNr=tab.TableNr";
+		String query = "SELECT br.RuleName, br.BusinessRuleNr, tab.TableName, col.ColumnName,  atcr.CompareValue, acrr.Operator, atcr.CompareType "
+				+ "FROM BusinessRule br, Attribute_Compare_rule atcr, Column col, Table tab " + "WHERE br.RuleNr =" + id
+				+ " AND br.RuleNr = atcr.RuleNr AND atcr.ColumNr=col.ColumnNr AND col.TableNr=tab.TableNr";
 		try (Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
 			Statement stmt = con.createStatement();
 			ResultSet dbResultSet = stmt.executeQuery(query);
 
 			while (dbResultSet.next()) {
-				br = new AttributeCompareRule(query, id, query, query);
-//				break;
+				br = new AttributeCompareRule(dbResultSet.getString("RuleName"), dbResultSet.getInt("BusinessRuleNr"),
+						dbResultSet.getString("TableName"), dbResultSet.getString("ColumnName"),
+						dbResultSet.getString("Operator"), dbResultSet.getInt("CompareValue"));
+				// break;
 			}
 
 			// ^database is nog niet compleet
